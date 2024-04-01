72014aec52272cef3cadb8240be3c8ad6024a176
Angelo Demaria <angelodemaria12@gmail.com>
2024-04-01 15:43:08 -0300

    cambios

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace AnalisisNumerico2024
+﻿namespace AnalisisNumerico2024.Unidad1
 {
-    partial class Form1
+    partial class Unidad1
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Bisección";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Entradas";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(593, 180);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Regla Falsa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(593, 276);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Tangente";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(593, 325);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Secante";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(580, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Cerrados";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(580, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Abiertos";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(53, 116);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(39, 23);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.Text = " F(x) =";
+            // 
+            // Unidad1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Unidad1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,74 @@
-namespace AnalisisNumerico2024
+using Calculus;
+
+namespace AnalisisNumerico2024.Unidad1
 {
-    public partial class Form1 : Form
+    public partial class Unidad1 : Form
     {
-        public Form1()
+        Calculo analizadorFuncion = new Calculo();
+
+        public void Prueba()
         {
-            InitializeComponent();
+
+            string function = input
+            double xi = 1;
+            double xd = 5;
+            double tolerancia = 0.0001;
+            int iteraciones = 100;
+
+
+            if (analizadorFuncion.Sintaxis(function, 'x'))
+            {
+                if (analizadorFuncion.EvaluaFx(xi) * analizadorFuncion.EvaluaFx(xd) > 0)
+                {
+                    MessageBox.Show("error varialble");
+                }
+                else
+                {
+                    var derivada = analizadorFuncion.Dx(xi);
+
+                    if (derivada < tolerancia || double.IsNaN(derivada))
+                    {
+                        MessageBox.Show("error tolerancia");
+                    }
+
+
+                    for (int i = 0; i <= iteraciones; i++)
+                    {
+                        if (Math.Abs(analizadorFuncion.EvaluaFx(xi)) < tolerancia)
+                        {
+                            MessageBox.Show("Raiz es 0");
+                        }
+
+                        else
+                        {
+                            MessageBox.Show("La sintasis es incorrecta");
+                        }
+
+                    }
+                }
+        }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
+
+
 }
