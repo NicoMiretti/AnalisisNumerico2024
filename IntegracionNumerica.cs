4097ba666fed33bd0487543f58b52d879eb7d615
aademaria <aademaria@COMPUT027750>
2024-06-11 19:27:10 -0300

    #

--- IntegracionNumerica.cs
+++ IntegracionNumerica.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculus;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,134 @@ namespace AnalisisNumerico2024
         public IntegracionNumerica()
         {
             InitializeComponent();
+        }
+
+        double CalcularIntegralTrapeciosSimple(string funcion, double xi, double xd)
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                return (Funcion.EvaluaFx(xi) + Funcion.EvaluaFx(xd)) * (xd - xi) / 2;
+            }
+            else
+            {
+                throw new Exception("Función Mal Ingresada");
+            }
+
+        }
+
+        private double CalcularIntegralTrapeciosMultiples(string funcion, double xi, double xd, int n)
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                double h = (xd - xi) / n;
+                double sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    sum += Funcion.EvaluaFx(xi + h * i);
+                }
+                return (h / 2) * (Funcion.EvaluaFx(xi) + 2 * sum + Funcion.EvaluaFx(xd));
+            }
+            else
+            {
+                throw new Exception("Función Mal Ingresada"); ;
+            }
+        }
+
+        private double CalcularIntegralSimpson1_3Simple(string funcion, double xi, double xd)
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                double h = (xd - xi) / 2;
+                return (h / 3) * (Funcion.EvaluaFx(xi) + 4 * Funcion.EvaluaFx(xi + h) + Funcion.EvaluaFx(xd));
+            }
+            else
+            {
+                throw new Exception("Función Mal Ingresada");
+            }
+        }
+
+        private double CalcularIntegralSimpson1_3Multiple(string funcion, double xi, double xd, int n)
+
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                double h = (xd - xi) / n;
+                double sumPares = 0, sumImpares = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sumPares += Funcion.EvaluaFx(xi + h * i);
+                    }
+                    else
+                    {
+                        sumImpares += Funcion.EvaluaFx(xi + h * i);
+                    }
+
+                }
+
+                return (h / 3) * (Funcion.EvaluaFx(xi) + 4 * sumImpares + 2 * sumPares + Funcion.EvaluaFx(xd));
+            }
+            else
+            {
+                throw new Exception("Función Mal Ingresada");
+            }
+        }
+
+        private double CalcularIntegralSimpson3_8(string funcion, double xi, double xd)
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                double h = (xd - xi) / 3;
+                return (3 * h / 8) * (Funcion.EvaluaFx(xi) + 3 * Funcion.EvaluaFx((xi + h)) + 3 * Funcion.EvaluaFx(xi + 2 * h) + Funcion.EvaluaFx(xd))
+            }
+            else
+                throw new Exception("Función Mal Ingresada");
+        }
+
+
+        private double CalcularIntegralAmbosMetodosSimpson(string funcion, double xi, double xd, int n)
+        {
+            Calculo Funcion = new Calculo();
+
+            if (Funcion.Sintaxis(funcion, 'x'))
+            {
+                double h = (xd - xi) / n;
+                double sumPares = 0, sumImpares = 0;
+                double resultado = 0;
+                bool simpson3_8Hecho = false;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (n % 2 != 0 && !simpson3_8Hecho)
+                    {
+                        double nuevoXi = xi + h * (n - 3);
+                        resultado = CalcularIntegralSimpson3_8(funcion, nuevoXi, xd);
+                        n = n - 3;
+                        simpson3_8Hecho = true;
+                    }
+                    if (i % 2 == 0)
+                    {
+                        sumPares += Funcion.EvaluaFx(xi + h * i);
+                    }
+                    else
+                    {
+                        sumImpares += Funcion.EvaluaFx(xi + h * i);
+                    }
+                }
+
+            }
         }
 
         private void label5_Click(object sender, EventArgs e)
